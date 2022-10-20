02528ad1699111e48d9712bc7d9a060c22a2c12b
Javier Gomez <javiergogar12@gmail.com>
2022-10-20 13:24:20 +0200

    signup hecho

--- easy-blend/public/js/model.js
+++ easy-blend/public/js/model.js
@@ -1,25 +1,65 @@
 Model = {} 
 Model.products = [{ 
   _id: 1, 
-  title: 'Super Mario 3D World and Bowser\'s Fury', 
-  description: '…', 
-  url: 'images/blend_pieces.jpg', 
-  price: 36.3, 
+  title: 'Trio blender', 
+  description: 'This is a pack to save some money', 
+  url: 'images/trio_blend.jpg', 
+  price: 180, 
   tax: 9.66 
 }, { 
   _id: 2, 
-  title: 'Splatoon 2', 
-  description: '…', 
-  url: 'img/2.jpg', 
-  price: 36.33, 
+  title: 'Pink blender', 
+  description: 'This is a nice and happy blender. If you use this you will brighten your days', 
+  url: 'images/pink_blend.jpg', 
+  price: 90, 
   tax: 9.66 
 },
 { 
   _id: 3, 
-  title: 'Splatoon 3', 
-  description: '…', 
-  url: 'img/2.jpg', 
-  price: 36.33, 
+  title: 'Pink Pro Blender', 
+  description: 'This is like Pink Blender but for professionals.', 
+  url: 'images/pink_pro_blend.jpg', 
+  price: 100, 
+  tax: 9.66 
+},
+{ 
+  _id: 4, 
+  title: 'Duo blender', 
+  description: 'Two for the price of one.', 
+  url: 'images/duo_blend.png', 
+  price: 90, 
+  tax: 9.66 
+},
+{ 
+  _id: 5, 
+  title: 'Duo blender 2', 
+  description: 'Two for the price of one, second version.', 
+  url: 'images/duo2_blend.jpg', 
+  price: 90, 
+  tax: 9.66 
+},
+{ 
+  _id: 6, 
+  title: 'Pack blender', 
+  description: 'Big pack blenders.', 
+  url: 'images/pack_blend.jpg', 
+  price: 280, 
+  tax: 9.66 
+},
+{ 
+  _id: 7, 
+  title: 'Blender pieces', 
+  description: 'Jack El Destripador´s favourite. Easy to assemble and disassemble.', 
+  url: 'images/blend_pieces.jpg', 
+  price: 90, 
+  tax: 9.66 
+},
+{ 
+  _id: 8, 
+  title: 'Green blender', 
+  description: 'This is a nice and cheap with a beautiful color blender but green. It´s very good for smoothies.', 
+  url: 'images/green_blend.jpg', 
+  price: 80, 
   tax: 9.66 
 }
 ]; 
@@ -37,19 +77,45 @@ Model.users = [{
   
 }];
 
+Model._userCount = Model.users.length;
+
 Model.signin = function (email, password) {
   
-  Model.user = null;
-  for (var i = 0; i < Model.users.length; i++) {
-  if (Model.users[i].email == email && Model.users[i].password == password)
-  Model.user = Model.users[i];
-  }
+    Model.user = null;
+    for (var i = 0; i < Model.users.length; i++) {
+    if (Model.users[i].email == email && Model.users[i].password == password)
+    Model.user = Model.users[i];
+    }
   }
 
 
 Model.signout = function(){
 
   Model.user = null;
-  
 
+}
+
+
+Model.signup = function (name, surname, address, birth, email, password) {
+  console.log(Model.users);
+  Model.user = null;
+  for (var i = 0; i < Model.users.length; i++) {
+      if (Model.users[i].email == email) {
+          console.log("This email is already registered.");
+          return null;
+      } else {
+          return Model.users.push({
+              '_id': Model._userCount++,
+              'email': email,
+              'password': password,
+              'name': name,
+              'surname': surname,
+              'birthdate': birth,
+              'address': address,
+              'cartItems': [],
+              'orders': [],
+          }
+          )
+      }
+  }
 }
